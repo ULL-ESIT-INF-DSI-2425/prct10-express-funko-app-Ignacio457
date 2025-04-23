5e41d5d7c075a1542727b4aa1f52b8548a2bf11d
Ignacio Gomez <alu0101464916@ull.edu.es>
2025-04-23 20:43:15 +0000

    arreglando2

--- tests/asyncFunctionsPromises.spec.ts
+++ tests/asyncFunctionsPromises.spec.ts
@@ -1,4 +1,21 @@
 import { describe, test, expect } from "vitest";
-import { }  from "../src/asyncFunctionsPromises.js";
+import { coordinatesInfoPromises, weatherInfoPromises } from "../src/asyncFunctionsPromises.js";
 
+
+describe("Asynchronous function coordinatesInfoPromises", () => {
+  test("coordinatesInfoPromises should get weather information", () => {
+    return coordinatesInfoPromises("Tenerife, Spain").then((data) => {
+      expect(data.body.features[0].geometry.coordinates).to.be.eql([
+        -13.670858, 28.920507,
+      ]);
+    });
+  });
+
+
+  test("weatherInfoPromises should provide an error", () => {
+    return coordinatesInfoPromises("12wherever").catch((err) => {
+      expect(err).to.be.equal("Mapbox API error: no location found");
+    });
+  });
+});
 
