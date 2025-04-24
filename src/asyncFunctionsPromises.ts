9a518d496e3d24419be9b7ef85c88fc159e67fcf
Ignacio Gomez <alu0101464916@ull.edu.es>
2025-04-24 10:21:52 +0000

    Modificacion

--- src/asyncFunctionsPromises.ts
+++ src/asyncFunctionsPromises.ts
@@ -1,47 +1,98 @@
-import request from "request";
+import request from 'request';
+
+
+type RequestType = {
+    name?: string,
+    gender?: string,
+    race?: string,
+    affiliation?: string
+}
+
+type CharacterType = {
+    id: number,
+    name: string,
+    ki: string,
+    maxKi: string,
+    race: string,
+    gender: string,
+    description: string,
+    image: string,
+    affiliation: string,
+    deletedAt: null
+}
+
 
 /**
  *
- * @param location - Location to look for lat/long coordinates in mapbox API
- * @returns A promise, which in case it is resolved, provides a response from the Mapbox API
+ * @param filtros - filtros para buscar el caracter
+ * @returns Una promesa, que devuelve el personaje que busacamos o un error si no existe
  */
-export const coordinatesInfoPromises = (location: string) => {
-  const url = `http://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(location)}&access_token=pk.eyJ1IjoiZWR1c2VncmUiLCJhIjoiY205MDY2aDZoMGo1ZDJocjA1ZTY2dHYyeCJ9.dJrmYDMmeVPotPhQPxeGBA&limit=1`;
-  return new Promise<request.Response>((resolve, reject) => {
-    request(
-      { url: url, json: true },
-      (error: Error, response: request.Response) => {
-        if (error) {
-          reject(error.message);
-        } else if (response.body.features.length === 0) {
-          reject("Mapbox API error: no location found");
-        } else {
-          resolve(response);
-        }
-      },
-    );
-  });
-};
 
-/**
- * 
- * @param location - Lat/long coordinates to look for weather info in weatherstack API
- * @returns A promise, which in case it is resolved, provides a response from the weatherstack API
- */
-export const weatherInfoPromises = (location: string) => {
-  const url = `http://api.weatherstack.com/current?access_key=81f7a73f7be745f0842e79d7cea739caca&query=${encodeURIComponent(location)}&units=m`;
-  return new Promise<request.Response>((resolve, reject) => {
-    request(
-      { url: url, json: true },
-      (error: Error, response: request.Response) => {
-        if (error) {
-          reject(error.message);
-        } else if (response.body.error) {
-          reject(response.body.error.type);
+export const DragonBallBusqueda = (filtros: RequestType) => {
+  return new Promise<CharacterType[]>((resolve, reject) => {   
+    let url = `https://dragonball-api.com/api/characters`;
+
+    let url_final: string = '';
+    let nombre: boolean = false;
+    let genero: boolean = false;
+    let raza: boolean = false;
+
+    if (filtros.name) {
+        url_final += `?name=${filtros.name}`;
+        nombre = true;
+    }
+
+    if (filtros.gender) {
+        if (nombre) {
+            url_final += `&gender=${filtros.gender}`
         } else {
-          resolve(response);
+            url_final += `?gender=${filtros.gender}`
         }
-      },
-    );
-  });
-}
+
+        genero = true;
+    }
+
+    if (filtros.race) {
+        if (nombre || genero) {
+            url_final += `&race=${filtros.race}`
+        } else {
+            url_final += `?race=${filtros.race}`
+        }
+
+        raza = true;
+    }    
+
+    if (filtros.affiliation) {
+        if (nombre || genero || raza) {
+            url_final += `&affiliation=${filtros.affiliation}`
+        } else {
+            url_final += `?affiliation=${filtros.affiliation}`
+        }
+    }  
+
+    url += url_final;
+    
+    request( { url: url, json: true }, (error: Error, response: request.Response) => {
+        if (error) {
+            reject(error.message);
+          } else if (response.body.length === 0) {
+            reject("No hay un personaje");
+          } else {
+            resolve(response);
+        }
+        },
+      );
+    });
+  };
+
+/* 
+  DragonBallBusqueda({ name: 'Goku' })
+  .then((funciona) => {
+      console.log(funciona);
+  })
+  .catch((error) => {
+      console.log(error);
+  })  
+*/
+
+
